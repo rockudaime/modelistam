372b023601633447d876d6a0d81c153758c9d9d0
Sergey <rockudaime@user.noreply.github.com>
2016-07-27 19:34:51 +0300

    Закончил с версткой и правками страниц overview, overiview-one

--- app/js/pages/overview.js
+++ app/js/pages/overview.js
@@ -1,20 +1,37 @@
 $(document).ready(function() {
 	if (document.querySelector('.o-compared-item__heading a')){
-		var el = document.querySelectorAll('.o-compared-item__heading a');
-		function ellipsis(obj, len) {
-			var text = obj.innerHTML;
-			if (text.length > len) {
-				text = text.slice(0, len - 3) + '&hellip;'; 
-				obj.innerHTML = text;
-			}
-		}
+		myTruncate(document.querySelectorAll('.o-compared-item__heading a'), 65); 
+		// var el = document.querySelectorAll('.o-compared-item__heading a');
+		// function ellipsis(obj, len) {
+		// 	var text = obj.innerHTML;
+		// 	if (text.length > len) {
+		// 		text = text.slice(0, len - 3) + '&hellip;'; 
+		// 		obj.innerHTML = text;
+		// 	}
+		// }
 		
-		for (var i = 0; i < el.length; i++){
-			ellipsis(el[i], 65);
-		}
+		// for (var i = 0; i < el.length; i++){
+		// 	ellipsis(el[i], 65);
+		// }
+	}
+
+	if (document.querySelector('.ov-similar-item__preview-text p')) {
+
+		myTruncate(document.querySelectorAll('.ov-similar-item__preview-text p'), 161); 
+		// var el = document.querySelectorAll('.ov-similar-item__preview-text p');
+		// function ellipsis(obj, len) {
+		// 	var text = obj.innerHTML;
+		// 	if (text.length > len) {
+		// 		text = text.slice(0, len - 3) + '&hellip;'; 
+		// 		obj.innerHTML = text;
+		// 	}
+		// }
+		
+		// for (var i = 0; i < el.length; i++){
+		// 	ellipsis(el[i], 65);
+		// }
 	}
 });
-
 
 $(document).ready(function() {
  
@@ -47,3 +64,18 @@ $(document).ready(function() {
 	}
 
 });
+
+function myTruncate(object_arr, len) {
+
+	function ellipsis(obj, len) {
+		var text = obj.innerHTML;
+		if (text.length > len) {
+			text = text.slice(0, len - 3) + '&hellip;'; 
+			obj.innerHTML = text;
+		}
+	}
+	
+	for (var i = 0; i < object_arr.length; i++){
+		ellipsis(object_arr[i], len);
+	}
+}
