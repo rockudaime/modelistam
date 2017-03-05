7393dda3895c63f73388e7b1dc5f382ac41986e5
Sergey <rockudaime@user.noreply.github.com>
2017-03-05 22:47:43 +0200

    Выпадающий список поиска. Небольшие изменения в шапке

--- app/js/pages/register.js
+++ app/js/pages/register.js
@@ -3,15 +3,53 @@ $(function() {
 	var signupForm = $('.signup-form');
 
 	signupForm.on('submit', function(event) {
-		checkPasswords();
+		var form = this;
+		var name = this.name;
+		var tel = this.tel;
+		var errorElem = document.createElement('p');
+		var valid = true;
+		var itm;
+		errorElem.classList.add('error-message', 'signup-form__error-message');
+
+		$('.error-message').remove();
+		$('.error').removeClass('error');
+
+		if (!tel.value) {
+			itm = errorElem.cloneNode(true);
+			itm.innerText = 'Телефон - обязательное поле';
+			tel.parentNode.insertBefore(itm, tel);
+			tel.classList.add('error');
+			valid = false;
+		}
+
+		if (!name.value) {
+			itm = errorElem.cloneNode(true);
+			itm.innerText = 'Имя - обязательное поле';
+			name.parentNode.insertBefore(itm, name);
+			name.classList.add('error');
+			valid = false;
+		}
+
+		var pass1 = document.getElementById("passwordl");
+	    var pass2 = document.getElementById("password2");
+	    if (!pass1.value || pass1.value !== pass2.value) {
+	    	itm = errorElem.cloneNode(true);
+			itm.innerText = 'Проверьте совпадют ли пароли';
+			pass1.parentNode.insertBefore(itm, pass1);
+			pass1.classList.add('error');
+			pass2.classList.add('error');
+			valid = false;
+	    }
+
+		return valid;
 	});
+
 	toggleLink.on('click', toggleLinkClickHandler);
 });
 
 function toggleLinkClickHandler (event)
 {
 	event.preventDefault();
-	console.log('clicked')
 
 	var toggleLink = $(this);
 	var contentBlock = $('#' + toggleLink.data('target-id'));
@@ -19,18 +57,3 @@ function toggleLinkClickHandler (event)
 	toggleLink.toggleClass('active');
 }
 
-function checkPasswords()
-{
-    var passl = document.getElementById("passwordl");
-    var pass2 = document.getElementById("password2");
-    var isMatch = passl.value === pass2.value;
-    console.log(isMatch);
-    if(!isMatch) {
-        passl.setCustomValidity("Пароли  не совпадают. Пожалуйста, проверьте  идентичность паролей в обоих полях!");
-    }
-    else {
-        passl.setCustomValidity("");
-    }
-
-    return isMatch;
-}
