f2b386d6b6d60a88ae35d920bf10a988d6b169a3
Sergey <bazarow0407@gmail.com>
2017-04-19 22:09:13 +0300

    Переработал скрипт по синхронизации слайдеров на странице сравнения

--- app/js/pages/compare.js
+++ app/js/pages/compare.js
@@ -1,123 +1,110 @@
 $(function() {
+  'use strict';
 
-	$('.mobile-custom-select').on('click', function(e) {
-		e.preventDefault();
-		var self = $(this);
-		$(this).toggleClass('active');
-		$(this).next().toggle();
-	});
+  $('.mobile-custom-select').on('click', function(e) {
+    e.preventDefault();
+    var self = $(this);
+    $(this).toggleClass('active');
+    $(this).next().toggle();
+  });
+
+  $('.compare-menu__item').on('click', function(e) {
+    e.preventDefault();
+
+    var targetClass = $(this).data('target-class'),
+      menuItem = $(this),
+      itemsTab = $('.compared-category'),
+      characteristicsTab = $('.compare-characheristics-tab');
+
+      itemsTab.filter('[data-class="' + targetClass +  '"]')
+              .add(menuItem)
+              .add(characteristicsTab.filter('[data-class="' + targetClass +  '"]'))
+              .addClass('active')
+              .siblings()
+              .removeClass('active');
+  });
+
+  
+  var viewedProductsSlider = $(".owl-compared-products");
+  viewedProductsSlider.owlCarousel({
+    loop: false,
+    margin:0,
+    navigationtext: false,
+    pagination: false,
+    nav: true,
+    dots: false,
+    scrollbar: true,
+
+    responsive:{
+      0:{
+        items:2,
+      },
+      544:{
+        items:2,
+      },
+      768:{
+        items:3,
+      },
+      1200:{
+        items:4,
+      }
+    }
+  });
+
+  var comparedProductsCharacteristics = $('.compare-table__characteristics-slider');
+
+  comparedProductsCharacteristics.owlCarousel({
+    loop: false,
+    margin:0,
+    navigationtext: false,
+    
+    pagination: false,
+    nav: false,
+    dots: false,
+    scrollbar: false,
+    touchDrag: false,
+    mouseDrag: false,
+
+    responsive:{
+      0:{
+        items:2,
+      },
+      544:{
+        items:2,
+      },
+      768:{
+        items:3,
+      },
+      1200:{
+        items:4,
+      }
+    }
+  });
+
+  viewedProductsSlider.each(function() {
+    console.log($(this).parent().data('class'));
+    var targetClass = $(this).parent().data('class');
+    var characteristicsTab = $('.compare-characheristics-tab[data-class="' + targetClass +  '"]');
+    var characteristicsSlider = characteristicsTab.find('.compare-table__characteristics-slider');
 
 
+    syncTwoSliders($(this), characteristicsSlider);
+  });
 
-	var tabs = new Tabs($('.compare-item'), $('.compare-menu__item'));
-	tabs.init();
+  // viewedProductsSlider.on('change.owl.carousel', function(event) {
+  //   if (event.namespace && event.property.name === 'position') {
+  //   var target = event.relatedTarget.relative(event.property.value, true);
+  //   comparedProductsCharacteristics.owlCarousel('to', target, 300, true);
+  //   }
+  // });
 
-	function Tabs($tabs, $tabLinks) {
-		var tabs = this;
-		tabs.elements = $tabs;
-		tabs.links = $tabLinks;
-
-		tabs.init = function () {
-			$tabLinks.on('click', showTargetTab);
-			$tabLinks.on('keypress', tabElemsKeypressHandler);
-		}
-
-		function showTargetTab (e) {
-			e.preventDefault();
-			var link = $(this);
-			var tabId = this.dataset.targetId;
-			var tab = $('#' + tabId);
-			var focus = tab.find('.focuspoint');
-
-			tabs.links.removeClass('active');
-			link.addClass('active');
-			tabs.elements.removeClass('active');
-			tab.addClass('active');
-			if (focus.length > 0) {
-				focus.focusPoint();
-			}
-
-			if ($(window).width() < 768) {
-				var linksContainer = tabs.links.parent()
-				mobileSelect = linksContainer.prev();
-				linksContainer.hide();
-				mobileSelect.removeClass('active');
-				mobileSelect.text($(this).text());
-			}
-		}
-	}
-
-
-
-
-	var viewedProductsSlider = $("#owl-viewed-products");
-	viewedProductsSlider.owlCarousel({
-		loop: false,
-		margin:0,
-		navigationtext: false,
-		dotsContainer: '#customDots',
-		
-		// responsiveclass:true,
-		pagination: false,
-		nav: true,
-		dots: false,
-		scrollbar: true,
-
-		responsive:{
-			0:{
-				items:2,
-			},
-			544:{
-				items:2,
-			},
-			768:{
-				items:3,
-			},
-			1200:{
-				items:4,
-			}
-		}
-	});
-
-	var comparedProductsCharacteristics = $('.compare-table__characteristics-slider');
-
-	comparedProductsCharacteristics.owlCarousel({
-		loop: false,
-		margin:0,
-		navigationtext: false,
-		
-		pagination: false,
-		nav: false,
-		dots: false,
-		scrollbar: false,
-		touchDrag: false,
-		mouseDrag: false,
-
-		responsive:{
-			0:{
-				items:2,
-			},
-			544:{
-				items:2,
-			},
-			768:{
-				items:3,
-			},
-			1200:{
-				items:4,
-			}
-		}
-	});
-
-	// viewedProductsSlider.on('change.owl.carousel', function(event) {
-	//   comparedProductsCharacteristics.trigger('to.owl.carousel', [event.item.index,300,true]);
-	// });
-
-	viewedProductsSlider.on('change.owl.carousel', function(event) {
-	  if (event.namespace && event.property.name === 'position') {
-	    var target = event.relatedTarget.relative(event.property.value, true);
-	    comparedProductsCharacteristics.owlCarousel('to', target, 300, true);
-	  }
-	});
+  function syncTwoSliders($slider, $sliderToSync) {
+    $slider.on('change.owl.carousel', function(event) {
+      if (event.namespace && event.property.name === 'position') {
+      var target = event.relatedTarget.relative(event.property.value, true);
+      $sliderToSync.owlCarousel('to', target, 300, true);
+      }
+    });
+  }
 });
 
